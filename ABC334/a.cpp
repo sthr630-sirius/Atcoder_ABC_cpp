276b5085352bde224d87d433668fb7fea19acf6c
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2024-02-12 14:07:14 +0900

    edit ABC334 level_a

---
+++ ABC334/a.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int b, g;
+    cin >> b >> g;
+    if (b > g){
+        cout << "Bat" << endl;
+    } else{
+        cout << "Glove" << endl;
+    }
+
+    return 0;
+    
+}
