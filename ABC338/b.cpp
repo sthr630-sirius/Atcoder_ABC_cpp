27aae5862ef0ce796f4d3e6835501c85d9aed790
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2024-02-24 09:34:07 +0900

    edit ABC338 level_b

--- ABC338/b.cpp
+++ ABC338/b.cpp
@@ -1,20 +1,35 @@
 #include<iostream>
 #include<map>
+#include<vector>
+#include<algorithm>
 using namespace std;
 int main(){
     string s;
+    char ans_key;
+    int ans_value;
     map<char, int> char_dict{};
 
     cin >> s;
+    ans_key = ' ';
+    ans_value = 0;
 
     for (int i=0; i<s.size(); i++){
         char_dict[s[i]]++;
     }
 
     //cout << char_dict['A'] << endl;
-    for (auto& [k, v] : char_dict){
-        cout << k << ":" << v << endl;     
+    //for (auto& [k, v] : char_dict){
+    //    cout << k << ":" << v << endl;     
+    //}
+
+    for (map<char, int>::iterator it = char_dict.begin(); it != char_dict.end(); it++){
+        if (it->second > ans_value){
+            ans_value = it->second;
+            ans_key = it->first;
+        }
     }
+
+    cout << ans_key << endl;
     
     return 0;
 
