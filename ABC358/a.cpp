0199dd133d74eb1ed78f25082e6e2224ed7a3728
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2024-07-07 09:30:37 +0900

    edit ABC358 level_a level_b

---
+++ ABC358/a.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s, t;
+    cin >> s >> t;
+    if (s=="AtCoder" and t == "Land") cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
