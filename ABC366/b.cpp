cce7122ba03e224eceb79a8bac8e8410a1560e9a
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2024-11-02 12:08:14 +0900

    level_a level_b passed test case

---
+++ ABC366/b.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    int m;
+    int j_max;
+    string t_str;
+    cin >> n;
+
+    vector<string> s(n);
+    vector<string> t;
+
+    m = 0;
+
+    for(int i=0; i<n; i++){
+        cin >> s[i];
+        if(s[i].length() >= m) m = s[i].length();
+        //m = max(m, (int)s[i].length());
+    }
+
+    for(int i=0; i<n; i++){
+        j_max = m - s[i].length();
+        for(int j=0; j<j_max; j++){
+            s[i].push_back('*');
+        }
+    }
+
+    reverse(s.begin(), s.end());
+
+    for(int j=0; j<m; j++){
+        t_str = "";
+        for(int i=0; i<n; i++){
+            t_str.push_back(s[i][j]);
+        }
+        t.push_back(t_str);
+    }
+
+    for(int i=0; i<m; i++){
+        for(int j=n-1; j>=0; j--){
+            if(t[i][j] == '*') t[i].pop_back();
+            else break;
+        }
+        cout << t[i] << endl;
+    }
+
+    return 0;
+
+}
